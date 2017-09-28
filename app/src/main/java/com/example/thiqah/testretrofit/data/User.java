b60db3d383b9408459daa0aa762b453c4a832907
iFahad94 <fahad.at94@gmail.com>
2017-09-28 13:53:17 +0100

    Test 12

--- app/src/main/java/com/example/thiqah/testretrofit/data/User.java
+++ app/src/main/java/com/example/thiqah/testretrofit/data/User.java
@@ -23,7 +23,7 @@ public class User {
         this.completed = completed;
     }
 
-    // test 11
+    // test 12
     public User(int userId, int id, String title) {
         this.userId = userId;
         this.id = id;
